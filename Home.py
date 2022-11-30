65735a9b385722ae5d0555c21b9ca4b808eae527
EscanoR <krichuzz@gmail.com>
2022-11-30 10:54:46 +0530

    created 2 pages

--- Home.py
+++ Home.py
@@ -2,14 +2,16 @@ import streamlit as kp
 import backe
 
 todos = backe.todo_open()
+kp.set_page_config(layout='wide')
 def adds_todo():
     newt=kp.session_state['newto']+'\n'
     todos.append(newt)
     backe.todo_write(todos)
 
 kp.title('My Todo App')
-kp.write('App increases productivity')
+kp.write("<b>App increases that increases productivity</b>",unsafe_allow_html=True)
 kp.subheader('My Todo App')
+kp.text_input(label='',placeholder='add a todo',on_change=adds_todo,key='newto')
 
 for index,todo in enumerate(todos):
     checkbox=kp.checkbox(todo,key=todo)
@@ -18,4 +20,4 @@ for index,todo in enumerate(todos):
         backe.todo_write(todos)
         del kp.session_state[todo]
         kp.experimental_rerun()
-kp.text_input(label='',placeholder='add a todo',on_change=adds_todo,key='newto')
+
---
+++ pages/About.py
@@ -0,0 +1,3 @@
+import streamlit as kp
+
+kp.write('<b>Page create for learning purpose</b>',unsafe_allow_html=True)
